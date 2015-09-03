df54b122dddbcdb94405c124531125ac028c18ca
BrayanAsdrubal <BrayanAsdrubal@Brayan>
2015-09-03 01:28:02 -0500

    Este es la practica número 1 entregable

--- src/practica1/Practica1.java
+++ src/practica1/Practica1.java
@@ -1,7 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this emplate file, choose Tools | Templates
- * and ope the template in the editor.
+ * and op the template in the editor.
  */
 package practica1;
 
