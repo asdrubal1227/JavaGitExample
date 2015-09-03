d9df26824bf371e18ae15a0b11ee0ac90d570c1a
BrayanAsdrubal <BrayanAsdrubal@Brayan>
2015-09-03 01:29:53 -0500

    Este es la practica número 1 entregable

--- src/practica1/Prestamo.java
+++ src/practica1/Prestamo.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open te template in the editor.
- */
 package practica1;
 
 import java.util.Scanner;
 
-/**
- *
- * @author practi10
- */
+
 public class Prestamo {
     
     private String cedula;
