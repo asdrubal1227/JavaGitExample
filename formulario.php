2972f1cfa7e45b8121dfa957acf7f86f00c119c1
asdrubal1227 <brayan_ahg@hotmail.com>
2015-09-09 07:58:43 -0500

    Create formulario.php

---
+++ formulario.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<!-- comentario -->
+	<head>
+	<meta charset="utf-8">
+
+
+	</head>
+	<body>//no se usan formularios anidados
+		<form action="forma1.php" method="post">
+Var1= <input  type="text" name="var1" value="valor"> 
+<br>//salto de linea
+Var2= <input  type="password" name="var2" > 
+<br>
+Var3= <input type="tel" name="telephone" id="telephone"  placeholder="(000)
+000-0000" > 
+<br>
+1.<input type="radio" checked name ='var4' value='1'>
+//Excluyentes poniendo el mismo nombre
+2.<input type="radio" name ='var4' value='2'>
+<br>
+<select name='lista'>
+	<option value='1'>
+		Uno
+	</option>
+	<option selected value='2'>
+		Dos
+	</option>
+	<option value='3'>
+		Tres
+	</option>
+</select>
+<input type="submit" value="Mandar"> //manda al formulario inmediatamente
+<input type="button">// hay que hacer previa configuracion
+<button>d</button>//igual que el segundo pero mas caracteristicas
+		</form>
+	</html>
+
+
+<form>
+
+</form>
